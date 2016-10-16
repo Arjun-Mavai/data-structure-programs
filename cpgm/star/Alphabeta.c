52951aa1728a782379ace776f8bbf8dcb4140218
arjunmac <arjungurjar786@gmail.com>
2016-10-16 17:19:24 +0000

    new program for printing alphabet

---
+++ cpgm/star/Alphabeta.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    
+    
+    for(i=0;i<26;++i)
+    {
+        char ch ='A' +i;
+        printf("%c " ,ch  );
+         
+        
+        
+    }
+    printf("\n");
+    for(i=0;i<26;++i)
+    {
+        char ch1 ='a'+i;
+        printf("%c ",ch1);
+    }
+    
+    
+	//code
+	return 0;
+}
